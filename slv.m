51dad06ef43b2b242646692da253a52b3a15e4fb
t-geon <88877637+t-geon@users.noreply.github.com>
2022-01-06 15:28:46 +0900

    Update slv.m

--- slv.m
+++ slv.m
@@ -29,20 +29,20 @@ for k=1:n
 end
 
 for k=1:n
-    s=0;    %s를 이용해서 Lc값을 저장하기 때문에 계속 0으로 초기화해줍니다..
+    s=0;    %Since the Lc value is stored, it is continuously initialized to 0.
     for j=1:k-1
-    s=s+L(k,j)*c(j);    %s에 L*c값을 저장합니다.(Lc=b에서 c를 구하기 위해서, L의 한 row에 column별로 c값(아직 정확히 알지 못합니다.)을 곱해서 더한 값을 s에 저장합니다.)
+    s=s+L(k,j)*c(j);   
     end
-    c(k)=b(k)-s;    %위에서 저장한 L*c를 b에 빼줘 나머지 값이 c 값이 됩니다.(L의 대각선 성분이 1이기 때문에), 즉 b를 elimination한 값을 c에 저장하는것 입니다.
+    c(k)=b(k)-s;    %Subtract from b and the remainder becomes the value of c.
 end
 
 for k=n:-1:1
-   t=0; %t를 사용해서 Ux값을 저장하기 때문에 계속 0으로 초기화 해줍니다.
+   t=0; %Since the Ux value is stored using t, it is continuously initialized to 0.
     for j=k+1:n 
-        t=t+U(k,j)*x(j);    % t에 Ux값을 저장합니다.(Ux=c를 이용해 x를 구하기 위해서, U의 한 행에 열 별로 x를 곱해서 더한 값을 t에 저장합니다.)
+        t=t+U(k,j)*x(j);    % Store the Ux value in t.
     end
-    x(k)=(c(k)-t)/U(k,k);   %위에서 c를 구한 방식과 같게 x를 구합니다.(Ux=c를 이용해서 위에서 구한 값을 c에 빼준후 U를 나눠 x값을 구합니다)
+    x(k)=(c(k)-t)/U(k,k);   %Find x in the same way that c was obtained above.
  
 end
-x=x';   %x가 1개의 행에 저장 되어있기 때문에 전치를 해줘야합니다.
+x=x';   %Since x is stored in one row, we need to transpose it.
 end
